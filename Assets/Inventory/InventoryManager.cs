ab9e2bdbe52a78206d95dbbfb1d28bd4eb6edd99
KillToRekt <148197279+KillToRekt@users.noreply.github.com>
2026-02-28 14:06:31 +0100

    test
    asd

--- Assets/Inventory/InventoryManager.cs
+++ Assets/Inventory/InventoryManager.cs
@@ -1,31 +1,59 @@
 using UnityEngine;
+using System.Collections.Generic; // Required for HashSet
+using System.Linq;               // Required for FirstOrDefault
+
+[System.Serializable]
+public class InventoryItem
+{
+    public string itemName;
+    public int quantity;
+}
 
 public class InventoryManager : MonoBehaviour
 {
     public GameObject InventoryMenu;
     private bool isInventoryOpen = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Use a List if you need to access items by index, 
+    // or stick with HashSet if you're managing unique references.
+    public List<InventoryItem> inventoryItems = new List<InventoryItem>();
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("e") && isInventoryOpen)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            Time.timeScale = 1;
-            InventoryMenu.SetActive(false);
-            isInventoryOpen = false;
+            ToggleInventory();
         }
+    }
 
-        else if (Input.GetKeyDown("e") && !isInventoryOpen)
+    void ToggleInventory()
+    {
+        isInventoryOpen = !isInventoryOpen;
+        InventoryMenu.SetActive(isInventoryOpen);
+
+        // Pauses game when open, resumes when closed
+        Time.timeScale = isInventoryOpen ? 0 : 1;
+    }
+
+    public void AddItem(string nameToAdd)
+    {
+        // Check if the item already exists in the list
+        InventoryItem existingItem = inventoryItems.FirstOrDefault(i => i.itemName == nameToAdd);
+
+        if (existingItem != null)
         {
-            Time.timeScale = 0;
-            InventoryMenu.SetActive(true);
-            isInventoryOpen = true;
+            existingItem.quantity++;
         }
+        else
+        {
+            // Add a brand new item if it wasn't found
+            inventoryItems.Add(new InventoryItem { itemName = nameToAdd, quantity = 1 });
+        }
+    }
+
+    // Check if an item exists and we have at least one
+    public bool HasItem(string nameToCheck)
+    {
+        return inventoryItems.Any(i => i.itemName == nameToCheck && i.quantity > 0);
     }
 }
