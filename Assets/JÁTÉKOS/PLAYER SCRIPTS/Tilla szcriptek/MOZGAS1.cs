d57cc76df8889e068fd8ebf20af1daa51059989b
Wallami21 <jozafattila@gmail.com>
2026-03-01 13:43:44 +0100

    Ez müködik, fut ugrik mindent is csinál
    Na szoval, vannak statjai, amikor fut a stamina stat csökken, csak akkor tud futni ha stamina stat 30 felett van, a stamina csak 5 mp elteltével kezd el nőni

--- Assets/JÁTÉKOS/PLAYER SCRIPTS/Tilla szcriptek/MOZGAS1.cs
+++ Assets/JÁTÉKOS/PLAYER SCRIPTS/Tilla szcriptek/MOZGAS1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
@@ -7,14 +9,15 @@ public class MOZGAS1 : MonoBehaviour
     Rigidbody rb;
     PALYERSTAT stat;
     Canvas canvas;
-    TextMeshProUGUI HP,St;
+    bool ON_OFF;
+    public TextMeshProUGUI HP,St;
     void Start()
     { 
         canvas = GetComponentInChildren<Canvas>();
-        HP = canvas.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
-        St = canvas.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI >();
+       
         stat = GetComponent<PALYERSTAT>();
-        InvokeRepeating(nameof(minuszstamina), 5f, 5f);
+        InvokeRepeating(nameof(minuszstamina), 1f, 1f);
+        
         rb = GetComponent<Rigidbody>();
     }
     bool hozzaer;
@@ -25,10 +28,14 @@ public class MOZGAS1 : MonoBehaviour
     float MozgasJB;
     public float jump = 1000f;
     public float gyorsasag;
-    bool fut;
+    bool fut, mehet;
     // Update is called once per frame
     void Update()
     {
+        
+         
+       
+
         if (stat.stamina <= 0)
         {
             gyorsasag = 5f;
@@ -37,42 +44,60 @@ public class MOZGAS1 : MonoBehaviour
         {
             gyorsasag = 10f;
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && stat.stamina >30 || mehet && Input.GetKey(KeyCode.LeftShift))
         {
             gyorsasag = gyorsasag*2;
             fut = true;
+            mehet = true;
         }
         else
         {
             fut = false;
+            mehet = false;
         }
         hozzaer = Physics.CheckSphere(GC.position, atmero, Ground);
 
         if (Input.GetKeyDown(KeyCode.Space) && hozzaer)
         {
             rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            stat.stamina -= 10; 
+            StopCoroutine(staminarecovery(5)); 
+            ON_OFF = false;
         }
 
         MozgasEH = Input.GetAxis("Horizontal");
         MozgasJB = Input.GetAxis("Vertical");
         Debug.Log("Grounded: " + hozzaer);
     }
-    
-    void FixedUpdate() { 
+
+    void FixedUpdate() {
         Vector3 move = transform.right * MozgasEH + transform.forward * MozgasJB;
         rb.MovePosition(rb.position + move * gyorsasag * Time.fixedDeltaTime);
         HP.text = $" HP :\t {stat.HP}";
         St.text = $" ST :\t {stat.stamina}";
-        minuszstamina();
+        
+        
     }
     void minuszstamina()
     {
-         ;
-        if (fut) { stat.stamina -= 20;}
-        else {  }
+        if (fut) { stat.stamina -= 3; StopCoroutine(staminarecovery(5)); ON_OFF = false; }
+        else { if (!ON_OFF) { ON_OFF = true; StartCoroutine(staminarecovery(5)); } }
         if (stat.stamina > 100) { stat.stamina = 100; }
-        if (stat.stamina < 0) { stat.stamina = 0; stat.HP = -1; }
+        if (stat.stamina < 0)
+        {
+            stat.stamina = 0;
+        }
+    }   
+    
 
+    IEnumerator staminarecovery(float sec)
+    {
+        
+        yield return new WaitForSeconds(sec);
+        while (!fut && stat.stamina <100) { stat.stamina += 1; yield return new WaitForSeconds(2f);}
+       
+        ON_OFF = false;
+        
     }
 }
 
